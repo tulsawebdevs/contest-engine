02bb23bcadc1f7084cbf2b10686e82bd0ec89fb2
BuddyL <buddy@sourcehold.com>
2011-10-08 13:12:42 -0500

    create new contest

--- app/controllers/contests_controller.rb
+++ app/controllers/contests_controller.rb
@@ -11,7 +11,20 @@ class ContestsController < ApplicationController
   end
 
   def create
-    
+    contest = Contest.new
+
+    contest.title = params[:title]
+    contest.description = params[:title]
+    contest.pdf = nil
+    contest.start_date = params[:start_date]
+    contest.end_date = params[:end_date]
+    contest.sponsor_id = nil
+
+    if(contest.save)
+      redirect_to "/contest/#{contest.id}/#{contest.title.gsub(' ', '-').downcase}"
+    else
+      redirect_to '/contest/new'
+    end
   end
 
   def edit
